55d7e5445e30dd44d9e4d9879413cd4d76f68e6a
Gnarlygav <ols15015@byui.edu>
2018-12-10 21:32:25 -0700

    starting with login reducer redus

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // REGISTER THE USER
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +15,38 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//login - get user token!
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //save to local storage
+      //extract it from param
+      const { token } = res.data;
+      //set it
+      localStorage.setItem('jwtToken', token);
+      //call the function that we have set up
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
