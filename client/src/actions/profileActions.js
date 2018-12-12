85501c5e648f28377afbb45a7346d8ad5be07cf6
Gnarlygav <ols15015@byui.edu>
2018-12-11 23:24:13 -0700

    setting up the dashboard and profile actions/reducers

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE
+} from './types';
+
+//GET THE current profile
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+
+  axios
+    .get('/api/profile')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+
+  //before, i had this, it's bad...
+  //    dispatch({
+  //    type: GET_PROFILE,
+  //    payload: err.data
+  //    })
+};
+
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
